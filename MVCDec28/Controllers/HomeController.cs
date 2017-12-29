121faf3aa075166c64a0e8ea0b236d2a8f062a61
Karnati <ravi-kiran.karnati@capgemini.com>
2017-12-29 16:58:31 +0530

    Add project files.

---
+++ MVCDec28/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDec28.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index(string id,string pass,string token)
+        {
+            return "HelloWorld @" + DateTime.Now.ToString();
+        }
+
+        public string Index1(string id)
+        {
+            return "HelloWorld1 @" + DateTime.Now.ToString();
+        }
+    }
+}
